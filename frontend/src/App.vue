e166480f2c18b1fb8721073e38b07595840f057b
Adarsha HD <adarshahd@users.noreply.github.com>
2024-04-16 20:56:12 +0530

    Initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import TheInitializationWrapper from './components/TheInitializationWrapper/TheInitializationWrapper.vue'
+</script>
+
+<template>
+  <Suspense>
+    <TheInitializationWrapper />
+    <template #fallback>
+      <div
+        class="flex
+            justify-center
+            items-center
+            h-full
+            w-full"
+      >
+        Loading...
+      </div>
+    </template>
+  </Suspense>
+</template>
